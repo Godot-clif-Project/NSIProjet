6d9ab352392cc8135854a155589ef404912c42a0
RetsuOwen <gabriel06juin@gmail.com>
2020-11-28 23:00:20 +0100

    Trying to add box appear animation (might not work)
    yeah um i hate working with animations and plus i hate yield() so much

--- GUI/Dialog.gd
+++ GUI/Dialog.gd
@@ -6,6 +6,8 @@ var JsonData
 var timer
 var dialogwait = 2
 var isTimerTimeout = 1
+var UpDialogBoxAppeared = false
+var DownDialogBoxAppeared = false
 
 func load_json(jsonname):
 	var file = File.new();
@@ -26,31 +28,46 @@ func proceed_dialog():
 	if line < JsonData.size():
 		if JsonData[line].position == 0:
 			if JsonData[line].command == 1:
-				get_node("UpBox").show()
-			$UpBox/UpDialogBox/UpPortraitMargin/UpPortraitTexture.texture = load(
-			"res://GUI/Portraits/" + JsonData[line].name + JsonData[line].emotion + ".png")
-			up_text()
+				showUpBox()
+			if UpDialogBoxAppeared == true:
+				$UpBox/UpDialogBox/UpPortraitMargin/UpPortraitTexture.texture = load(
+				"res://GUI/Portraits/" + JsonData[line].name + JsonData[line].emotion + ".png")
+				up_text()
 		elif JsonData[line].position == 1:
 			if JsonData[line].command == 1:
-				get_node("DownBox").show()
-			$DownBox/DownDialogBox/DownPortraitMargin/DownPortraitTexture.texture = load(
-			"res://GUI/Portraits/" + JsonData[line].name + JsonData[line].emotion + ".png")
-			down_text()
-			
+				showDownBox()
+			if DownDialogBoxAppeared == true:
+				$DownBox/DownDialogBox/DownPortraitMargin/DownPortraitTexture.texture = load(
+				"res://GUI/Portraits/" + JsonData[line].name + JsonData[line].emotion + ".png")
+				down_text()
 	else:
+		$UpBox/UpAnimation.play("Disappear")
+		$DownBox/DownAnimation.play("Disappear")
+		yield($UpBox/UpAnimation,"animation_finished")
+		yield($DownBox/DownAnimation,"animation_finished")
 		queue_free()
 	line += 1
 
 func up_text():
 	$UpBox/UpDialogBox/UpTextMargin/UpText.bbcode_text = JsonData[line].text
-	$UpBox/UpDialogBox/UpNameMargin/UpName.bbcode_text = JsonData[line].name
+	$UpBox/UpDialogBox/NinePatchRect/UpNameMargin/UpName.bbcode_text = "[center]"+JsonData[line].name
 	$UpBox/UpDialogBox/UpTextMargin/UpText.percent_visible = 0
 	$UpBox/UpDialogBox/Tween.interpolate_property($UpBox/UpDialogBox/UpTextMargin/UpText, "percent_visible", 0,1,0.4, Tween.TRANS_SINE,Tween.EASE_IN)
 	$UpBox/UpDialogBox/Tween.start()
 
-func down_text():
-	$DownBox/DownDialogBox/DownTextMargin/DownText.bbcode_text = JsonData[line].text
-	$DownBox/DownDialogBox/DownNameMargin/DownName.bbcode_text = "[right]"+JsonData[line].name
+func down_text(): 
+	$DownBox/DownDialogBox/DownTextMargin/DownText.bbcode_text = "[right]"+JsonData[line].text
+	$DownBox/DownDialogBox/NinePatchRect2/DownNameMargin/DownName.bbcode_text = "[center]"+JsonData[line].name
 	$DownBox/DownDialogBox/DownTextMargin/DownText.percent_visible = 0
 	$DownBox/DownDialogBox/Tween.interpolate_property($DownBox/DownDialogBox/DownTextMargin/DownText, "percent_visible", 0,1,0.4, Tween.TRANS_SINE,Tween.EASE_IN)
 	$DownBox/DownDialogBox/Tween.start()
+
+func showUpBox():
+	get_node("UpBox").show()
+	$UpBox/UpAnimation.play("Appear")
+	UpDialogBoxAppeared = true
+
+func showDownBox():
+	get_node("DownBox").show()
+	$DownBox/DownAnimation.play("Appear")
+	DownDialogBoxAppeared = true
