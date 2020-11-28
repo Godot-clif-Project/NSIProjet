310c80ff9c595684f1a0ab1247d3bb4d4b16bfb7
RetsuOwen <gabriel06juin@gmail.com>
2020-11-28 16:02:24 +0100

    Text parser funsies
    - "command" now is used ("1" makes the dialog box appear, "0" is for simple text)
    - "emotion" loads files for the portraits based on the name + emotion.
    - "position" : "0" will edit the top box, "1" will edit the bottom box".
    
    Dialog scene now has 2 boxes + Box texture (the box texture is also a separate scene to edit more easily, even though we don't really need it lol)

--- GUI/Dialog.gd
+++ GUI/Dialog.gd
@@ -15,6 +15,8 @@ func load_json(jsonname):
 	
 func _ready():
 	load_json("test")
+	get_node("UpBox").hide()
+	get_node("DownBox").hide()
 
 func _process(delta):
 	if Input.is_action_just_pressed("Debug1"):
@@ -22,11 +24,33 @@ func _process(delta):
 
 func proceed_dialog():
 	if line < JsonData.size():
-		$TextLabel.bbcode_text = JsonData[line].text
-		$NameLabel.bbcode_text = JsonData[line].name
-		$TextLabel.percent_visible = 0
-		$Tween.interpolate_property($TextLabel, "percent_visible", 0,1,0.4, Tween.TRANS_SINE,Tween.EASE_IN)
-		$Tween.start()
+		if JsonData[line].position == 0:
+			if JsonData[line].command == 1:
+				get_node("UpBox").show()
+			$UpBox/UpDialogBox/UpPortraitMargin/UpPortraitTexture.texture = load(
+			"res://GUI/Portraits/" + JsonData[line].name + JsonData[line].emotion + ".png")
+			up_text()
+		elif JsonData[line].position == 1:
+			if JsonData[line].command == 1:
+				get_node("DownBox").show()
+			$DownBox/DownDialogBox/DownPortraitMargin/DownPortraitTexture.texture = load(
+			"res://GUI/Portraits/" + JsonData[line].name + JsonData[line].emotion + ".png")
+			down_text()
+			
 	else:
 		queue_free()
 	line += 1
+
+func up_text():
+	$UpBox/UpDialogBox/UpTextMargin/UpText.bbcode_text = JsonData[line].text
+	$UpBox/UpDialogBox/UpNameMargin/UpName.bbcode_text = JsonData[line].name
+	$UpBox/UpDialogBox/UpTextMargin/UpText.percent_visible = 0
+	$UpBox/UpDialogBox/Tween.interpolate_property($UpBox/UpDialogBox/UpTextMargin/UpText, "percent_visible", 0,1,0.4, Tween.TRANS_SINE,Tween.EASE_IN)
+	$UpBox/UpDialogBox/Tween.start()
+
+func down_text():
+	$DownBox/DownDialogBox/DownTextMargin/DownText.bbcode_text = JsonData[line].text
+	$DownBox/DownDialogBox/DownNameMargin/DownName.bbcode_text = "[right]"+JsonData[line].name
+	$DownBox/DownDialogBox/DownTextMargin/DownText.percent_visible = 0
+	$DownBox/DownDialogBox/Tween.interpolate_property($DownBox/DownDialogBox/DownTextMargin/DownText, "percent_visible", 0,1,0.4, Tween.TRANS_SINE,Tween.EASE_IN)
+	$DownBox/DownDialogBox/Tween.start()
