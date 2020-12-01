bc5bdd7d1eb55680ed5cb33c7726bb91ade9e8e0
RetsuOwen <gabriel06juin@gmail.com>
2020-12-01 01:46:11 +0100

    text parser almost finished!
    now it can load dialogue via NPCs! (also edited the testlevel)
    ...let's not talk about my code because it works, it's functional and atleast
    you didn't have to code it yourself :sunglasses:

--- GUI/Dialog.gd
+++ GUI/Dialog.gd
@@ -11,11 +11,14 @@ var DownDialogBoxAppeared = false
 var jsonname = "errorhandler"
 
 func load_json(jsonname):
+	line = 0
 	var file = File.new();
 	file.open("res://GUI/"+jsonname+".json", file.READ);
 	JsonData = parse_json(file.get_as_text())
 	file.close()
-	
+
+func _init():
+	pass
 func _ready():
 	load_json(str(jsonname))
 	get_node("UpBox").hide()
@@ -28,32 +31,37 @@ func _process(delta):
 func proceed_dialog():
 	if line < JsonData.size():
 		if JsonData[line].position == 0:
-			if JsonData[line].command == 1:
-				showUpBox()
-				yield($UpBox/UpAnimation,"animation_finished")
-			if UpDialogBoxAppeared == true:
-				$UpBox/UpDialogBox/UpPortraitMargin/UpPortraitTexture.texture = load(
-				"res://GUI/Portraits/" + JsonData[line].name + JsonData[line].emotion + ".png")
-				up_text()
-				yield($UpBox/UpDialogBox/Tween,"tween_completed")
+			UpBoxHandler(JsonData[line].command)
+#			if JsonData[line].command == 1:
+#				if UpDialogBoxAppeared == false:
+#					showUpBox()
+#					yield($UpBox/UpAnimation,"animation_finished")
+#			if UpDialogBoxAppeared == true and $UpBox.TweenComplete == true:
+#				if $DownBox/DownDialogBox/DownTextMargin/DownText.percent_visible == 0 or $DownBox/DownDialogBox/DownTextMargin/DownText.percent_visible == 100:
+#					up_text()
+#					line += 1
+		# Same code but for the Down Box. please help
 		elif JsonData[line].position == 1:
-			if JsonData[line].command == 1:
-				showDownBox()
-				yield($DownBox/DownAnimation,"animation_finished")
-			if DownDialogBoxAppeared == true:
-				$DownBox/DownDialogBox/DownPortraitMargin/DownPortraitTexture.texture = load(
-				"res://GUI/Portraits/" + JsonData[line].name + JsonData[line].emotion + ".png")
-				down_text()
-				yield($DownBox/DownDialogBox/Tween,"tween_completed")
+			DownBoxHandler(JsonData[line].command)
+#			if JsonData[line].command == 1:
+#				if DownDialogBoxAppeared == false:
+#					showDownBox()
+#					yield($DownBox/DownAnimation,"animation_finished")
+#			if DownDialogBoxAppeared == true and $DownBox.TweenComplete == true:
+#				if $UpBox/UpDialogBox/UpTextMargin/UpText.percent_visible == 0 or $UpBox/UpDialogBox/UpTextMargin/UpText.percent_visible == 100:
+#					down_text()
+#					line += 1
 	else:
 		$UpBox/UpAnimation.play("Disappear")
-		$DownBox/DownAnimation.play("Disappear")
 		yield($UpBox/UpAnimation,"animation_finished")
+		$DownBox/DownAnimation.play("Disappear")
 		yield($DownBox/DownAnimation,"animation_finished")
-		queue_free()
-	line += 1
+		ResetBoxes()
 
 func up_text():
+	$UpBox.TweenComplete == false
+	$UpBox/UpDialogBox/UpPortraitMargin/UpPortraitTexture.texture = load(
+	"res://GUI/Portraits/" + JsonData[line].name + JsonData[line].emotion + ".png")
 	$UpBox/UpDialogBox/UpTextMargin/UpText.bbcode_text = JsonData[line].text
 	$UpBox/UpDialogBox/NinePatchRect/UpNameMargin/UpName.bbcode_text = "[center]"+JsonData[line].name
 	$UpBox/UpDialogBox/UpTextMargin/UpText.percent_visible = 0
@@ -61,6 +69,9 @@ func up_text():
 	$UpBox/UpDialogBox/Tween.start()
 
 func down_text(): 
+	$DownBox.TweenComplete == false
+	$DownBox/DownDialogBox/DownPortraitMargin/DownPortraitTexture.texture = load(
+	"res://GUI/Portraits/" + JsonData[line].name + JsonData[line].emotion + ".png")
 	$DownBox/DownDialogBox/DownTextMargin/DownText.bbcode_text = "[right]"+JsonData[line].text
 	$DownBox/DownDialogBox/NinePatchRect2/DownNameMargin/DownName.bbcode_text = "[center]"+JsonData[line].name
 	$DownBox/DownDialogBox/DownTextMargin/DownText.percent_visible = 0
@@ -76,3 +87,41 @@ func showDownBox():
 	$DownBox/DownAnimation.play("Appear")
 	get_node("DownBox").show()
 	DownDialogBoxAppeared = true
+
+func UpBoxHandler(command):
+	if command == 1:
+		if UpDialogBoxAppeared == false:
+			showUpBox()
+			yield($UpBox/UpAnimation,"animation_finished")
+	if UpDialogBoxAppeared == true and $UpBox.TweenComplete == true:
+		#if $DownBox/DownDialogBox/DownTextMargin/DownText.percent_visible == 100:
+		up_text()
+	else:
+		$UpBox/UpAnimation.play("Disappear")
+		yield($UpBox/UpAnimation,"animation_finished")
+#		$UpBox.queue_free()
+	line += 1
+
+func DownBoxHandler(command):
+	if command == 1:
+		if DownDialogBoxAppeared == false:
+			showDownBox()
+			yield($DownBox/DownAnimation,"animation_finished")
+	if DownDialogBoxAppeared == true and $DownBox.TweenComplete == true:
+		#if $UpBox/UpDialogBox/UpTextMargin/UpText.percent_visible == 100:
+		down_text()
+	else:
+		$DownBox/DownAnimation.play("Disappear")
+		yield($DownBox/DownAnimation,"animation_finished")
+#		$DownBox.queue_free()
+	line += 1
+
+func ResetBoxes():
+	line = 0
+	get_node("UpBox").hide()
+	get_node("DownBox").hide()
+	DownDialogBoxAppeared = false
+	UpDialogBoxAppeared = false
+	$DownBox/DownDialogBox/DownTextMargin/DownText.percent_visible = 0
+	$UpBox/UpDialogBox/UpTextMargin/UpText.percent_visible = 0
+	load_json("test")
