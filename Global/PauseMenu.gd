c0eda113cfebcb69208b65630524079993efb646
RetsuOwen <gabriel06juin@gmail.com>
2020-12-12 00:12:12 +0100

    uwu menu

--- Global/PauseMenu.gd
+++ Global/PauseMenu.gd
@@ -6,9 +6,14 @@ var paused_update: bool
 onready var bg = $Background
 onready var tree = get_tree()
 
+onready var optionsmenu = get_node("Options")
+onready var mainpause = get_node("Main")
+
 
 func _ready():
 	visible = false
+	mainpause.hide()
+	optionsmenu.hide()
 
 
 func _process(delta):
@@ -16,10 +21,12 @@ func _process(delta):
 		if not paused_update:
 			visible = true
 			paused_update = true
+			mainpause.show()
 	elif paused_update:
 		visible = false
 		paused_update = false
 
+# Main Menu Buttons
 
 func _on_Resume_gui_input(event):
 	if Input.is_action_just_pressed("menu_confirm"):
@@ -28,9 +35,17 @@ func _on_Resume_gui_input(event):
 
 func _on_Options_gui_input(event):
 	if Input.is_action_just_pressed("menu_confirm"):
-		print("Option menu appear")
+		mainpause.hide()
+		optionsmenu.show()
 
 
 func _on_Quit_gui_input(event):
 	if Input.is_action_just_pressed("menu_confirm"):
 		tree.quit()
+
+# Options Menu Buttons
+
+func _on_Return_gui_input(event):
+	if Input.is_action_just_pressed("menu_confirm"):
+		optionsmenu.hide()
+		mainpause.show()
